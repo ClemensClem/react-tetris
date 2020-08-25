3a6c592c8f3792dfb43dabd12fcf42f1108b5fec
Clemens Broehl <clemens.broehl@posteo.de>
2020-08-25 21:12:34 +0200

    styling

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrominos";
 
 export default function Cell({ type }) {
-    return <div>cell</div>;
+    return (
+        <StyledCell type={type} color={TETROMINOS[type].color}>
+            cell
+        </StyledCell>
+    );
 }
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
 import { createStage } from "../gameHelpers";
 
@@ -9,16 +10,18 @@ import StartButton from "./StartButton";
 
 export default function Tetris() {
     return (
-        <div>
-            <Stage stage={createStage()} />
-            <aside>
-                <div>
-                    <Display text="Score" />
-                    <Display text="Rows" />
-                    <Display text="Level" />
-                </div>
-                <StartButton />
-            </aside>
-        </div>
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={createStage()} />
+                <aside>
+                    <div>
+                        <Display text="Score" />
+                        <Display text="Rows" />
+                        <Display text="Level" />
+                    </div>
+                    <StartButton />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     );
 }
